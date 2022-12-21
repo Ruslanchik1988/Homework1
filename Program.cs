8e6fab7dd93cec96d0d5c78bf8dd695a68738926
Ruslan <kirito7379@gmail.com>
2022-12-21 19:59:57 +0300

    Задача на два числа

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5;
+int b = 7;
+
+if(a > b)
+{
+    Console.Write($"Max = {a}\n");
+}
+else
+{
+    Console.Write($"Max = {b}\n");
+}
+if(a > b)
+{
+    Console.Write($"Min = {b}\n");
+}
+else
+{
+    Console.Write($"Min = {a}\n");
+}
